fc7b7b5f2818452d9f958378705e3e39e2bb7acf
Kevin Chen <55222995+kevc528@users.noreply.github.com>
2020-09-18 00:22:44 -0500

    Finish database loading

--- bootstrap.py
+++ bootstrap.py
@@ -3,19 +3,40 @@ import json
 from app import db, DB_FILE
 from models import *
 
-def user_signup(username, name):
-    new_user = User(username, name)
+def user_signup(username, name, favorites):
+    new_user = User(username, name, favorites)
     db.session.add(new_user)
     db.session.commit()
+    # db.session.refresh(new_user)
+    # print(new_user.id)
 
 
 def create_user():
-    user_signup('josh', 'Josh')
+    user_signup('josh', 'Josh', [])
 
 
 def load_data():
-    print('hello')
+    with open('clubs.json') as fObj:
+        clubs = json.load(fObj)
+    tag_map = {}
+    for club in clubs:
+        tag_objs = []
+        for tag in club['tags']:
+            if tag not in tag_map:
+                tag_obj = Tag(tag)
+                tag_map[tag] = tag_obj
+                db.session.add(tag_obj)
+                db.session.commit()
+            tag_objs.append(tag_map[tag])
+        db.session.add(Club(club['code'], club['name'], club['description'], tag_objs))
+    db.session.commit()
+
 
 # No need to modify the below code.
 if __name__ == '__main__':
+    if os.path.exists(DB_FILE):
+        os.remove(DB_FILE)
+    
+    db.create_all()
+    create_user()
     load_data()
--- models.py
+++ models.py
@@ -4,28 +4,46 @@ from app import db
 # Check out the Flask-SQLAlchemy quickstart for some good docs!
 # https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
 
+tag_relations = db.Table('tag_relations',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+    db.Column('club_id', db.Integer, db.ForeignKey('club.id'), primary_key=True)
+)
+
 class Club(db.Model):
     # make the id column because I'm not sure if the code can change or not
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(80), unique=True, nullable=False)
     name = db.Column(db.String(120), unique=True, nullable=False)
     description = db.Column(db.Text)
+    tags = db.relationship('Tag', secondary=tag_relations)
+
+    def __init__(self, code, name, description, tags):
+        self.code = code
+        self.name = name
+        self.description = description
+        self.tags = tags
 
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tag_name = db.Column(db.String(80), unique=True, nullable=False)
 
-tag_relations = db.Table('tag_relations',
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
-    db.Column('club_id', db.Integer, db.ForeignKey('club.id'), primary_key=True)
-)
+    def __init__(self, tag_name):
+        self.tag_name = tag_name
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    name = db.Column(db.String(120), unique=True, nullable=False)
 
 favorites = db.Table('favorites',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('club_id', db.Integer, db.ForeignKey('club.id'), primary_key=True)
 )
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Club', secondary=favorites)
+
+    def __init__(self, username, name, favorites):
+        self.username = username
+        self.name = name
+        self.favorites = favorites
