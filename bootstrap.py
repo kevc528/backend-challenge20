337579acd2eb42f1c26b6ab8da323836741f4823
Kevin Chen <kevc528@seas.upenn.edu>
2020-09-17 22:30:20 -0500

    models

--- bootstrap.py
+++ bootstrap.py
@@ -1,21 +1,21 @@
 import os
+import json
 from app import db, DB_FILE
+from models import *
+
+def user_signup(username, name):
+    new_user = User(username, name)
+    db.session.add(new_user)
+    db.session.commit()
+
 
 def create_user():
-    print("TODO: Create a user called josh")
+    user_signup('josh', 'Josh')
+
 
 def load_data():
-    from models import *
-    print("TODO: Load in clubs.json to the database.")
-
-
+    print('hello')
 
 # No need to modify the below code.
 if __name__ == '__main__':
-    # Delete any existing database before bootstrapping a new one.
-    if os.path.exists(DB_FILE):
-        os.remove(DB_FILE)
-
-    db.create_all()
-    create_user()
     load_data()
--- models.py
+++ models.py
@@ -3,3 +3,29 @@ from app import db
 # Your database models should go here.
 # Check out the Flask-SQLAlchemy quickstart for some good docs!
 # https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+
+class Club(db.Model):
+    # make the id column because I'm not sure if the code can change or not
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.Text)
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag_name = db.Column(db.String(80), unique=True, nullable=False)
+
+tag_relations = db.Table('tag_relations',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+    db.Column('club_id', db.Integer, db.ForeignKey('club.id'), primary_key=True)
+)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+favorites = db.Table('favorites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('club_id', db.Integer, db.ForeignKey('club.id'), primary_key=True)
+)
