9a31e932882cb9c2a8e361a8d3ef4668564a8dda
Kevin Chen <55222995+kevc528@users.noreply.github.com>
2020-09-21 22:06:14 -0500

    name can duplicate

--- models.py
+++ models.py
@@ -66,7 +66,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    name = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(80), nullable=False)
     year = db.Column(db.Integer)
     major = db.Column(db.String(80))
